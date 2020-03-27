79a33845961eacaa7cf6bbdf01ef58637f2303ac
root <root@localhost.localdomain>
2020-03-27 22:56:41 +0530

    first code commit

---
+++ cpuusage.py
@@ -0,0 +1,17 @@
+import os
+import psutil
+print("enter any value")
+c= None
+default = None
+print(c)
+if c==None: print('none')
+else: print('blah')
+c= input()
+if not c:
+    c=default
+else:
+    print(c)
+if c== None:
+    print("yes")
+else:
+    print("no")
